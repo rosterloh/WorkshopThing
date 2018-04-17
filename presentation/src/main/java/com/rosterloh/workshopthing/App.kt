65d4cb94b3021284ffe8a5ecb65db32317bd48d4
Richard Osterloh <richard.osterloh@gmail.com>
2018-04-17 15:19:42 +0100

    Basic structure compiling

--- presentation/src/main/java/com/rosterloh/workshopthing/App.kt
+++ presentation/src/main/java/com/rosterloh/workshopthing/App.kt
@@ -1,10 +1,23 @@
 package com.rosterloh.workshopthing
 
+import com.crashlytics.android.Crashlytics
 import com.rosterloh.workshopthing.di.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
+import io.fabric.sdk.android.Fabric
+import timber.log.Timber
 
 class App : DaggerApplication() {
+
     override fun applicationInjector(): @JvmSuppressWildcards AndroidInjector<out DaggerApplication> =
             DaggerAppComponent.builder().create(this)
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        } else {
+            Fabric.with(this, Crashlytics())
+        }
+    }
 }
--- things/src/main/java/com/rosterloh/workshopthing/ui/main/MainViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.rosterloh.workshopthing.ui.main
-
-import android.arch.lifecycle.ViewModel
-
-class MainViewModel : ViewModel() {
-
-}
